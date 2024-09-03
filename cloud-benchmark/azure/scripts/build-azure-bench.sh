165d28f27fe56170892b5e11b083ea40d9d8acfa
Tim Greene <tim.g.greene@gmail.com>
2024-09-03 11:51:12 +0100

    Setup yourkit on aks

--- cloud-benchmark/azure/scripts/build-azure-bench.sh
+++ cloud-benchmark/azure/scripts/build-azure-bench.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 set -e
+
+# Use the first argument passed in, or default to 'xtdb-azure-bench'
+image_name=${1:-xtdb-azure-bench}
+
 (
     ../../gradlew :cloud-benchmark:azure:shadowJar
 
     sha=$(git rev-parse --short HEAD)
 
     echo Building Docker image ...
-    docker build -t xtdb-azure-bench:latest --platform=linux/amd64 --build-arg GIT_SHA="$sha" --build-arg XTDB_VERSION="${XTDB_VERSION:-dev-SNAPSHOT}" .
+    docker build -t "$image_name":latest --platform=linux/amd64 --build-arg GIT_SHA="$sha" --build-arg XTDB_VERSION="${XTDB_VERSION:-dev-SNAPSHOT}" .
     echo Done
 )
