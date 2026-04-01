7887ca577e885857a30879bc96c5b08f1c29db66
Tim Greene <tim.g.greene@gmail.com>
2026-04-01 16:26:33 +0100

    test(grafana): add composition tests for table discovery query
    Tests the function composition patterns that Grafana's PostgreSQL
    plugin uses for table discovery.

--- src/test/clojure/xtdb/pgwire/grafana_test.clj
+++ src/test/clojure/xtdb/pgwire/grafana_test.clj
@@ -12,3 +12,19 @@
   (t/testing "double-dash inside string literals is not treated as a comment"
     (t/is (= [{:bar "--foo"}]
              (xt/q tu/*node* "SELECT '--foo' AS bar")))))
+
+(t/deftest test-grafana-function-composition
+  (t/testing "quote_ident on parse_ident round-trip"
+    (t/is (= [{:x "\"My Schema\""}]
+             (xt/q tu/*node* "SELECT quote_ident(parse_ident('\"My Schema\".table_name')[1]) AS x"))
+          "quoted part from parse_ident re-quoted by quote_ident")
+
+    (t/is (= [{:x "table_name"}]
+             (xt/q tu/*node* "SELECT quote_ident(parse_ident('\"My Schema\".table_name')[2]) AS x"))
+          "unquoted part from parse_ident stays unquoted through quote_ident"))
+
+  (t/testing "Grafana table discovery query pattern"
+    (t/is (= [{:x " public"}]
+             (xt/q tu/*node*
+                   "SELECT string_to_array(current_setting('search_path'), ',')[2] AS x"))
+          "extracting second element from search_path")))
